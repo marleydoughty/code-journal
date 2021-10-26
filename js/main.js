f75e1ed3688cf94d08a30ed47e9d38a661929ea5
Marley Doughty <marleydoughty@yahoo.com>
2021-10-25 18:01:59 -0700

    working on submit event and input object

--- js/main.js
+++ js/main.js
@@ -2,8 +2,26 @@
 /* exported data */
 var $photoUrl = document.querySelector('#photo-url');
 var $imageTag = document.querySelector('img');
+// var $save = document.querySelector('.button');
+// var $title = document.querySelector('#title');
+// var $notes = document.querySelector('#notes');
 
 function handleInput(event) {
   $imageTag.setAttribute('src', event.target.value);
 }
 $photoUrl.addEventListener('input', handleInput);
+
+var $formInputs = document.querySelector('#form-inputs');
+function handleSubmit(event) {
+  event.preventDefault();
+  // var formData = {
+  //   title: $formInputs.elements.title.value,
+  //   photoUrl: $formInputs.elements.photoUrl.value,
+  //   notes: $formInputs.elements.notes.value
+  // };
+  // console.log('form data:', formData);
+  // $imageTag.reset();
+  // $title.reset();
+  // $notes.reset();
+}
+$formInputs.addEventListener('submit', handleSubmit);
