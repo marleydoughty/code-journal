4838d44a74a123f045e9c0e5f4b66efbfa60d5ed
Marley Doughty <marleydoughty@yahoo.com>
2021-10-26 13:07:40 -0700

    verified local storage and incrementing entry ID

--- js/data.js
+++ js/data.js
@@ -6,3 +6,12 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+var previousData = localStorage.getItem('data-local-storage');
+if (previousData !== null) {
+  data = JSON.parse(previousData);
+}
+function stringifyData(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-local-storage', dataJSON);
+}
+window.addEventListener('beforeunload', stringifyData);
