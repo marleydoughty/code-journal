05f34b9bdd4646511b68349cbc818ad385afc094
Marley Doughty <marleydoughty@yahoo.com>
2021-10-25 17:10:47 -0700

    input event on photoUrl and updated src attribute value

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+var $photoUrl = document.querySelector('#photo-url');
+var $imageTag = document.querySelector('img');
+
+function handleInput(event) {
+  $imageTag.setAttribute('src', event.target.value);
+}
+$photoUrl.addEventListener('input', handleInput);
