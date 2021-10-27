1cc3637a7de630656ce8a18f2907a80b350b141f
Marley Doughty <marleydoughty@yahoo.com>
2021-10-27 13:30:08 -0700

    working on loop for each journal entry

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 var $photoUrl = document.querySelector('#photo-url');
 var $imageTag = document.querySelector('img');
 var $formInputs = document.querySelector('#form-inputs');
-
+// var $newEntryButton = document.querySelector('.new-entry-button');
 // var $save = document.querySelector('.button');
 // var $title = document.querySelector('#title');
 // var $notes = document.querySelector('#notes');
@@ -29,29 +29,36 @@ function handleSubmit(event) {
 
 $formInputs.addEventListener('submit', handleSubmit);
 
-// function journalEntry(event) {
-//   var $allEntries = document.createElement('ul');
-//   $allEntries.setAttribute('id', 'view-entries');
-//   var $entry = document.createElement('li');
-//   $entry.className = 'view';
-//   $allEntries.appendChild($entry);
-//   var $row = document.createElement('div');
-//   $row.className = 'row';
-//   $entry.appendChild($row);
-//   var $column1 = document.createElement('div');
-//   $column1.className = 'column-half';
-//   $row.appendChild($column1);
-//   var $photo = document.createElement('img');
-//   $photo.setAttribute('src', $formInputs.elements['photo-url'].value);
-//   $column1.appendChild($photo);
-//   var $column2 = document.createElement('div');
-//   $column2.className = 'column-half';
-//   $row.appendChild($column2);
-//   var $title = document.createElement('h4');
-//   $title.textContent = $formInputs.elements.title.value;
-//   $column2.appendChild($title);
-//   var $notes = document.createElement('p');
-//   $notes.textContent = $formInputs.elements.notes.value;
-//   $column2.appendChild($notes);
-//   return $allEntries;
-// }
+function journalEntry(event) {
+  var $allEntries = document.createElement('ul');
+  $allEntries.setAttribute('id', 'view-entries');
+  var $entry = document.createElement('li');
+  $entry.className = 'view';
+  $allEntries.appendChild($entry);
+  var $row = document.createElement('div');
+  $row.className = 'row';
+  $entry.appendChild($row);
+  var $column1 = document.createElement('div');
+  $column1.className = 'column-half';
+  $row.appendChild($column1);
+  var $photo = document.createElement('img');
+  $photo.setAttribute('src', $formInputs.elements['photo-url'].value);
+  $column1.appendChild($photo);
+  var $column2 = document.createElement('div');
+  $column2.className = 'column-half';
+  $row.appendChild($column2);
+  var $title = document.createElement('h4');
+  $title.textContent = $formInputs.elements.title.value;
+  $column2.appendChild($title);
+  var $notes = document.createElement('p');
+  $notes.textContent = $formInputs.elements.notes.value;
+  $column2.appendChild($notes);
+  return $allEntries;
+}
+var currentEntries = document.querySelector('.row.justify-content');
+
+for (var i = 0; i < data.entries.length; i++) {
+  var newEntry = journalEntry(data.entries[i]);
+  currentEntries.appendChild(newEntry);
+}
+window.addEventListener('DOMContentLoaded', journalEntry);
