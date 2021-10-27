a030c0f6f112d304301806fdad9f312618c394cc
Marley Doughty <marleydoughty@yahoo.com>
2021-10-27 11:56:18 -0700

    defined function that created DOM tree for single journal entry

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 /* exported data */
 var $photoUrl = document.querySelector('#photo-url');
 var $imageTag = document.querySelector('img');
+var $formInputs = document.querySelector('#form-inputs');
+
 // var $save = document.querySelector('.button');
 // var $title = document.querySelector('#title');
 // var $notes = document.querySelector('#notes');
@@ -11,7 +13,6 @@ function handleInput(event) {
 }
 $photoUrl.addEventListener('input', handleInput);
 
-var $formInputs = document.querySelector('#form-inputs');
 function handleSubmit(event) {
   event.preventDefault();
   var formData = {
@@ -27,3 +28,30 @@ function handleSubmit(event) {
 }
 
 $formInputs.addEventListener('submit', handleSubmit);
+
+// function journalEntry(event) {
+//   var $allEntries = document.createElement('ul');
+//   $allEntries.setAttribute('id', 'view-entries');
+//   var $entry = document.createElement('li');
+//   $entry.className = 'view';
+//   $allEntries.appendChild($entry);
+//   var $row = document.createElement('div');
+//   $row.className = 'row';
+//   $entry.appendChild($row);
+//   var $column1 = document.createElement('div');
+//   $column1.className = 'column-half';
+//   $row.appendChild($column1);
+//   var $photo = document.createElement('img');
+//   $photo.setAttribute('src', $formInputs.elements['photo-url'].value);
+//   $column1.appendChild($photo);
+//   var $column2 = document.createElement('div');
+//   $column2.className = 'column-half';
+//   $row.appendChild($column2);
+//   var $title = document.createElement('h4');
+//   $title.textContent = $formInputs.elements.title.value;
+//   $column2.appendChild($title);
+//   var $notes = document.createElement('p');
+//   $notes.textContent = $formInputs.elements.notes.value;
+//   $column2.appendChild($notes);
+//   return $allEntries;
+// }
